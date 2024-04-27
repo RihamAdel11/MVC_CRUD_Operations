273ee8d6b66908513ea5385963c6abfbe8bb3212
Riham <rihammohareb6@gmail.com>
2024-04-28 02:09:55 +0300

    Upgrade  .Net5 to .Net8

--- Demo.PLL/Program.cs
+++ Demo.PLL/Program.cs
@@ -1,5 +1,16 @@
+using Demo.BLL.Interfaces;
+using Demo.BLL.Repositry;
+using Demo.DAL.Context;
+using Demo.DAL.Models;
+using Demo.PLL.MappingProfiles;
+using Demo.PLL.Services.EmailSender;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,14 +24,53 @@ namespace Demo.PLL
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
-		}
+			var webApplicationBuilder = WebApplication.CreateBuilder(args);
+			#region ConfigureServices
+			webApplicationBuilder.Services.AddControllersWithViews();
+			webApplicationBuilder.Services.AddDbContext<MVCAppContext>(options =>
+			options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection")));
+			webApplicationBuilder.Services.AddScoped<IDepartmentRepositry, DepartmentRepositry>();
+			webApplicationBuilder.Services.AddTransient<IEmailSender, EmailSender>();
+			webApplicationBuilder.Services.AddScoped<IUnitWork, UnitOfWork>();
+			webApplicationBuilder.Services.AddAutoMapper(M => M.AddProfile(new EmployeeProfile()));
+			webApplicationBuilder.Services.AddAutoMapper(M => M.AddProfile(new DepartmentProfile()));
+			webApplicationBuilder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<MVCAppContext>().AddDefaultTokenProviders();
+			webApplicationBuilder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+			{
+				options.LoginPath = "Account/Login";
+				options.AccessDeniedPath = "Home/Error";
+			});
+			#endregion
+			var app = webApplicationBuilder.Build();
+			#region Configure Kesterel MiddelWare
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
+			app.UseRouting();
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Account}/{action=Login}/{id?}");
+			});
+			#endregion
+			app.Run();
+		}
+	
+		
 	}
 }
--- Demo.PLL/Startup.cs
+++ Demo.PLL/Startup.cs
@@ -32,50 +32,13 @@ namespace Demo.PLL
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews();
-			services.AddDbContext<MVCAppContext>(options =>
-			options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-			services.AddScoped<IDepartmentRepositry, DepartmentRepositry>();
-			services.AddTransient <IEmailSender,EmailSender >();	
-			services.AddScoped<IUnitWork, UnitOfWork>();
-			services.AddAutoMapper(M => M.AddProfile(new EmployeeProfile()));
-			services.AddAutoMapper(M => M.AddProfile(new DepartmentProfile()));
-			services.AddIdentity<ApplicationUser, IdentityRole>()
-				.AddEntityFrameworkStores<MVCAppContext>().AddDefaultTokenProviders();
-			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
-			{
-				options.LoginPath = "Account/Login";
-				options.AccessDeniedPath = "Home/Error";
-			});
+			
 		}
-		
+
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-
-			app.UseRouting();
-			app.UseAuthentication();
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Account}/{action=Login}/{id?}");
-			});
 		}
 	}
 }
